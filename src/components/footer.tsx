c888727eaebf75696369bc667bc9d28632b79397
Matt <director@acidgambit.com>
2024-01-31 10:56:54 -0500

    spacing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from "./mode-toggle";
 export function Footer() {
   return (
     <div className="bg-background/60 fixed bottom-0 left-0 p-4 flex justify-between w-full">
-      <footer className="flex h-10 w-full shrink-0 items-center px-4 md:px-6 py-8">
+      <footer className="flex h-10 w-full shrink-0 items-center px-4 md:px-6">
         <div className="mr-auto">
           <div>
             <h1 className="font-mono text-sm">2024 Matt Wilder</h1>
